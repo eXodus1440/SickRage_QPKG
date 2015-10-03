9e4859353da5e38413137e05daf7e2b857ef5f83
Steve Bauer <stev8541@mm7155fd56.home>
2015-10-03 20:53:03 +0100

    Update init script

--- shared/SickRage.sh
+++ shared/SickRage.sh
@@ -5,15 +5,43 @@ CMD_SETCFG="/sbin/setcfg"
 
 QPKG_NAME="SickRage"
 QPKG_ROOT=$(${CMD_GETCFG} ${QPKG_NAME} Install_Path -f ${CONF})
-PYTHON_DIR="/opt/bin"
+PYTHON_DIR="/usr/bin"
 #PATH="${QPKG_ROOT}/bin:${QPKG_ROOT}/env/bin:${PYTHON_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
-PYTHON="${PYTHON_DIR}/python2.6"
-
+PYTHON="${PYTHON_DIR}/python2.7"
 SICKRAGE="${QPKG_ROOT}/SickBeard.py"
-#QPKG_DATA=${QPKG_ROOT}/.sickbeard
-#QPKG_CONF=${QPKG_DATA}/settings.conf
-#WEBUI_PORT=$(${CMD_GETCFG} core port -f ${QPKG_CONF})
-#QPKG_PID=${QPKG_ROOT}/couchpotato-${WEBUI_PORT}.pid
+QPKG_DATA=${QPKG_ROOT}/.sickbeard
+QPKG_CONF=${QPKG_DATA}/config.ini
+WEBUI_PORT=$(${CMD_GETCFG} core port -f ${QPKG_CONF})
+if [ -z ${WEBUI_PORT} ] ; then WEBUI_PORT="8086" ; fi # Default to port 5050
+QPKG_PID=${QPKG_ROOT}/couchpotato-${WEBUI_PORT}.pid
+
+start_daemon() {
+  #PATH=${PATH} ${PYTHON} ${SICKRAGE} --daemon --pid_file ${QPKG_PID} --data_dir=${QPKG_DATA} --config ${QPKG_CONF}
+  ${PYTHON} ${SICKRAGE} --daemon --pidfile=${QPKG_PID} --datadir=${QPKG_DATA} --config=${QPKG_CONF}
+}
+
+stop_daemon() {
+  kill $(cat ${QPKG_PID})
+  wait_for_status 1 20
+  if [ -f ${QPKG_PID} ] ; then rm -f ${QPKG_PID} ; fi
+}
+
+daemon_status() {
+  if [ -f ${QPKG_PID} ] && [ -d /proc/$(cat ${QPKG_PID} 2>/dev/null) ]; then
+    return 0
+  fi
+  return 1
+}
+
+wait_for_status() {
+  counter=$2
+  while [ ${counter} -gt 0 ]; do
+    daemon_status
+    [ $? -eq $1 ] && break
+    let counter=counter-1
+    sleep 1
+  done
+}
 
 case "$1" in
   start)
@@ -22,11 +50,40 @@ case "$1" in
         echo "$QPKG_NAME is disabled."
         exit 1
     fi
-    : ADD START ACTIONS HERE
+
+    if daemon_status; then
+      echo "${QPKG_NAME} is already running"
+    else
+      #echo "Checking if ${QPKG_NAME} is linked to SABnzbdPlus"
+      #${QPKG_ROOT}/link_to_SAB.sh
+      echo "Starting ${QPKG_NAME} ..."
+      start_daemon
+    fi
     ;;
 
   stop)
-    : ADD STOP ACTIONS HERE
+    if daemon_status; then
+      echo "Stopping ${QPKG_NAME} ..."
+      stop_daemon
+    else
+      echo "${QPKG_NAME} is not running"
+    fi
+    ;;
+
+  status)
+    if daemon_status; then
+      echo "${QPKG_NAME} is running"
+      exit 0
+    else
+      echo "${QPKG_NAME} is not running"
+      exit 1
+    fi
+    ;;
+
+  relink)
+    ${CMD_SETCFG} core linked_to_sabnzbd 0 -f ${QPKG_CONF}
+    echo "relinking ${QPKG_NAME} to SABnzbdPlus"
+    ${QPKG_ROOT}/link_to_SAB.sh
     ;;
 
   restart)
