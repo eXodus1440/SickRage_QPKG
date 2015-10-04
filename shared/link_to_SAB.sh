5d941faefece9e91cb277cd24a55a9a1e2d40d70
Steve Bauer <stev8541@mm7155fd56.home>
2015-10-04 09:51:44 +0100

    updated SAB linking

--- shared/link_to_SAB.sh
+++ shared/link_to_SAB.sh
@@ -8,7 +8,7 @@ MULTIMEDIA=$($CMD_GETCFG SHARE_DEF defMultimedia -d Multimedia -f /etc/config/de
 
 SYS_QPKG_DIR=$($CMD_GETCFG SickRage Install_Path -f /etc/config/qpkg.conf)
 SAB_INSTALLED=$($CMD_GETCFG SABnzbdPlus Status -f /etc/config/qpkg.conf)
-SAB_LINKED=$($CMD_GETCFG General linked_to_sabnzbd -f ${SYS_QPKG_DIR}/.sickrage/config.ini)
+SAB_LINKED=$($CMD_GETCFG General linked_to_sabnzbd -f ${SYS_QPKG_DIR}/.sickrage/sabnzbd_link.ini)
 
 # Exit if SickRage is already linked with SABnzbdPlus
 if [ -n ${SAB_LINKED} ] && [ "${SAB_LINKED}" = "1" ] ; then 
@@ -52,28 +52,38 @@ if [ "$SAB_INSTALLED" == "complete" ] ; then
     SABnzbdPlus_WEBUI_IP=$($CMD_GETCFG misc host -f ${SABnzbdPlus_Path}/.sabnzbd/sabnzbd.ini)
     if [ "$SABnzbdPlus_WEBUI_HTTPS" = "0" ]; then
       SABnzbdPlus_WEBUI_PORT=$($CMD_GETCFG misc port -f ${SABnzbdPlus_Path}/.sabnzbd/sabnzbd.ini)
-      $CMD_SETCFG SABnzbd sab_host http://${SABnzbdPlus_WEBUI_IP}:${SABnzbdPlus_WEBUI_PORT} -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+      $CMD_SETCFG SABnzbd sab_host http://${SABnzbdPlus_WEBUI_IP}:${SABnzbdPlus_WEBUI_PORT}/ -f ${SYS_QPKG_DIR}/.sickrage/config.ini
     else
       SABnzbdPlus_WEBUI_PORT=$($CMD_GETCFG misc https_port -f ${SABnzbdPlus_Path}/.sabnzbd/sabnzbd.ini)
       #$CMD_SETCFG SABnzbd ssl 1 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
-      $CMD_SETCFG SABnzbd sab_host https://${SABnzbdPlus_WEBUI_IP}:${SABnzbdPlus_WEBUI_PORT} -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+      $CMD_SETCFG SABnzbd sab_host https://${SABnzbdPlus_WEBUI_IP}:${SABnzbdPlus_WEBUI_PORT}/ -f ${SYS_QPKG_DIR}/.sickrage/config.ini
     fi
+    SABnzbdPlus_USER=$($CMD_GETCFG misc username -f ${SABnzbdPlus_Path}/.sabnzbd/sabnzbd.ini)
+    SABnzbdPlus_PASS=$($CMD_GETCFG misc password -f ${SABnzbdPlus_Path}/.sabnzbd/sabnzbd.ini)
     SABnzbdPlus_APIKEY=$($CMD_GETCFG misc api_key -f ${SABnzbdPlus_Path}/.sabnzbd/sabnzbd.ini)
 
     # Set SABnzbdPlus values in SickRage 
     #$CMD_SETCFG SABnzbd enabled 1 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG SABnzbd sab_username ${SABnzbdPlus_USER} -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG SABnzbd sab_password ${SABnzbdPlus_PASS} -f ${SYS_QPKG_DIR}/.sickrage/config.ini
     $CMD_SETCFG SABnzbd sab_apikey ${SABnzbdPlus_APIKEY} -f ${SYS_QPKG_DIR}/.sickrage/config.ini
     $CMD_SETCFG SABnzbd sab_category TV -f ${SYS_QPKG_DIR}/.sickrage/config.ini
 
-    # Set Renamer values based on SABnzbdPlus values
+    # Set Post Processing values based on SABnzbdPlus values
     [ -d ${BASE}/${MULTIMEDIA}/TV ] || $CMD_MKDIR -p ${BASE}/${MULTIMEDIA}/TV
-    #$CMD_SETCFG renamer enabled 1 -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
-    #$CMD_SETCFG renamer from ${BASE}/${PUBLIC_SHARE}/Downloads/complete -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
-    #$CMD_SETCFG renamer to ${BASE}/${MULTIMEDIA}/Movies -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
-    #$CMD_SETCFG renamer cleanup 1 -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
+    $CMD_SETCFG General enabled 1 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General tv_download_dir ${BASE}/${PUBLIC_SHARE}/Downloads/complete/TV -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General process_method move -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General process_automatically 1 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General create_missing_show_dirs 1 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General use_failed_downloads 0 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    #$CMD_SETCFG General delete_failed 1 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
 
     # Set a few defaults, assuming connecting into SABnzbdPlus and not Torrent
     $CMD_SETCFG General launch_browser 0 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General use_nzbs 1 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General use_torrents 0 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General nzb_method sabnzbd -f ${SYS_QPKG_DIR}/.sickrage/config.ini
     #$CMD_SETCFG blackhole enabled 0 -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
     #$CMD_SETCFG kickasstorrents enabled 0 -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
     #$CMD_SETCFG torrentz enabled 0 -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
@@ -81,11 +91,13 @@ if [ "$SAB_INSTALLED" == "complete" ] ; then
 
     # Disable the SickRage Updater and setup Wizard
     $CMD_SETCFG General auto_update 0 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General version_notify 0 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    
     #$CMD_SETCFG updater automatic 0 -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
     #$CMD_SETCFG core show_wizard 0 -f ${SYS_QPKG_DIR}/.couchpotato/settings.conf
 
     # Set SickRage as linked to SABnzbdPlus
-    $CMD_SETCFG General linked_to_sabnzbd 1 -f ${SYS_QPKG_DIR}/.sickrage/config.ini
+    $CMD_SETCFG General linked_to_sabnzbd 1 -f ${SYS_QPKG_DIR}/.sickrage/sabnzbd_link.ini
   fi
 fi
 
